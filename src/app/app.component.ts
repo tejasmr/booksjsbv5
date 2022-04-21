a51ee34d940f57e5a8fe4d99914e4078e733f242
DESKTOP-2EK6EMA\totej <totejasmr@gmail.com>
2022-04-21 21:17:48 +0530

    Added toast for sort

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -84,6 +84,11 @@ export class AppComponent implements OnInit {
   }
   async sortBook($event: Sort) {
     this.dataSource.data = await this.bookService.sorted($event);
-    console.log(this.dataSource.data);
+    this.snackBar.open(
+      'Successfully sorted', 'Dismiss', {
+        duration: 2000,
+        panelClass: ['success']
+      }
+    );
   }
 }
